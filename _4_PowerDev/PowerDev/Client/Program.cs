002f606cd58112fcfd2d17cb04599371052d8ff2
marek2222 <marekmusial2@o2.pl>
2021-03-26 18:43:02 +0100

    CodeBehind

--- _4_PowerDev/PowerDev/Client/Program.cs
+++ _4_PowerDev/PowerDev/Client/Program.cs
@@ -17,7 +17,8 @@ namespace Power_Dev.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { 
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
